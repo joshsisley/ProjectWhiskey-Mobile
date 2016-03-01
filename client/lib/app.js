12db85cf5334c0526fa44e4861b5d30fb4e2e3ed
Joshua Sisley <joshua.sisley@gmail.com>
2016-02-29 19:17:21 -0600

    working on getting the scanning to work

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,6 +1,29 @@
 /**
  * Created by Joshua on 2/27/2016.
  */
-if (Meteor.isClient) {
-    angular.module('whiskyMobile', ['angular-meteor', 'ui.router', 'accounts.ui']);
+angular.module('whiskyMobile', ['angular-meteor', 'ui.router', 'accounts.ui', 'ngCordova']);
+
+if (Meteor.isCordova) {
+    angular.element(document).on("deviceready", onReady);
+    document.addEventListener("deviceready", onDeviceReady, false);
+    cordova.plugins.barcodeScanner.scan( //<------------- The code breaks at the word cordova
+        function (result) {
+            alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+} else {
+    angular.element(document).ready(onReady);
+}
+
+
+function onReady() {
+    angular.bootstrap(document, ['whiskyMobile'], {
+        strictDi: true
+    });
 }
--- client/register/register.js
+++ client/register/register.js
@@ -1,7 +1,19 @@
 /**
  * Created by Joshua on 2/27/2016.
  */
-angular.module('whiskyMobile').controller('registerController', function($scope) {
+angular.module('whiskyMobile').controller('registerController', function($scope, $cordovaBarcodeScanner) {
+
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner
+            .scan()
+            .then(function(barcodeData) {
+                console.log("success");
+                console.log(barcodeData);
+            }, function(error) {
+                console.log("did not work, oh no :(")
+            });
+    };
+
     $scope.submitRegistration = function() {
         Accounts.createUser({
             email: $scope.registerEmail,
